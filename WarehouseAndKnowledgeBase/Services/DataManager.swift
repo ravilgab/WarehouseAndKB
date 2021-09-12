474158f98f3b19c1985155b9b1963dc1b37fe9cf
Ravil <ravilgab@yahoo.com>
2021-09-12 19:12:16 +0300

    Prepared project for MVC

---
+++ WarehouseAndKnowledgeBase/Services/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  WarehouseAndKnowledgeBase
+//
+//  Created by Ravil on 12.09.2021.
+//
+
+class DataManager {
+    
+    let kbTitles = ["SCCM fix",
+                    "Как изменить свою фамилию в почте",
+                    "Если компьютер выбило из домена",
+                    ""]
+}
